ec85212d19ded4cefc5aafda1303b3305081f1db
sophiapinto <sarahsophiapinto@hotmail.com>
2024-06-03 15:04:47 -0300

    commit initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './assets/logo.png';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
+      </header>
+
+      <footer>
+        
+      </footer>
+    </div>
+  );
+}
+
+export default App;
